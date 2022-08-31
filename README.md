05c128def4c3823b9cb8dd865f0c47cb9968ccbb
Laurent Senta <laurent@singulargarden.com>
2022-08-31 12:45:14 +0200

    basic: pagination + logging

---
+++ README.md
@@ -0,0 +1,13 @@
+# ipdx/stats
+
+## Setup
+
+```
+echo 'export GH_TOKEN="0xxxxx" > ./secrets.sh'
+```
+
+## Usage
+
+```
+source ./secrets.sh && npm run build && npm run main 1>lines.jsonvi
+```
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { Console } from 'console'
-import 'reflect-metadata'
-import { GitHub } from './github'
+import { Console } from 'console';
+import 'reflect-metadata';
+import { GitHub } from './github';
 
 const OWNER = 'laurentsenta'
 const REPO = 'test-plans'
 const BRANCH = 'laurent-statistics-3008'
-const RUNS_LIMIT = 2000
-const DEBUG = false
+const DEBUG = process.env.DEBUG === 'true'
 
 interface IWorkflowLine {
   workflowId: number,
@@ -40,144 +39,169 @@ interface IJobStepLine extends IJobLine {
 
 const debug = new Console(process.stderr)
 
+const DoneErr = new Error('done')
+
 // https://docs.github.com/en/rest/actions/workflow-runs#get-workflow-run-usage
 async function run(): Promise<void> {
   const gh = await GitHub.get()
+
+  // TODO: should be a plugin or middleware
+  const allOf = gh.client.paginate.iterator
+  const actions = gh.client.actions
 
   const workflowLines: IWorkflowLine[] = []
   const jobLines: IJobLine[] = []
   const stepLines: IJobStepLine[] = []
 
-  // Naive debug implementation, TODO: throw or use generators instead.
-  const isDone = (): boolean => {
+  // https://github.com/octokit/plugin-paginate-rest.js/#how-it-works
+  // TODO: implement and use 
+  // async function* allOf<R extends OctokitTypes.RequestInterface>(q: R, parameters: Parameters<R>[0]): AsyncIterableIterator<NormalizeResponse<OctokitTypes.GetResponseTypeFromEndpointMethod<R>>> {
+  //   for await (const response of gh.client.paginate.iterator(
+  //     q, parameters
+  //   )) {
+  //     console.log('response', response)
+
+  //     for (const item of response.data) {
+  //       yield item
+  //     }
+  //   }
+  // }
+
+  // TODO: fix, naive
+  const throwWhenDone = () => {
     if (!DEBUG) {
-      return false;
+      return;
     }
-
-    if (jobLines.length > 10 || workflowLines.length > 10) {
-      return true; // debug with 10 lines for now
+    if (workflowLines.length > 10) {
+      throw DoneErr
     }
-    if (stepLines.length > 10) {
-      return true; // debug with 10 lines for now
+    if (jobLines.length > 10) {
+      throw DoneErr
     }
-
-    return false;
+    if (stepLines.length > 100) {
+      throw DoneErr
+    }
   }
 
-  const workflowsRequest = await gh.client.actions.listRepoWorkflows({
-    owner: OWNER,
-    repo: REPO,
-  })
-
-  const workflows = workflowsRequest.data.workflows;
-  debug.log(`Found ${workflows.length} workflows.`)
-
-  for (const workflow of workflows) {
-    debug.log(`Fetching workflow ${workflow.id} (${workflow.name})`)
-
-    const workflowRunsRequest = await gh.client.actions.listWorkflowRuns({
-      owner: OWNER,
-      repo: REPO,
-      branch: BRANCH,
-      workflow_id: workflow.id,
-      limit: RUNS_LIMIT,
-    })
-    const workflowRuns = workflowRunsRequest.data.workflow_runs;
-
-    debug.log(`Found ${workflowRuns.length} workflow runs.`)
-    debug.log("a workflow looks like this:", workflowRuns[0])
-
-    for (const workflowRun of workflowRuns) {
-      debug.log(`Fetching run ${workflowRun.id}`)
-
-      const usageRequest = await gh.client.actions.getWorkflowRunUsage({
+  async function* allWorkflows() {
+    for await (const response of allOf(
+      actions.listRepoWorkflows,
+      {
         owner: OWNER,
         repo: REPO,
-        run_id: workflowRun.id,
-      })
-
-      const usage = usageRequest.data;
-
-      const workflowLine = {
-        workflowId: workflow.id,
-        workflowName: workflow.name,
-        headBranch: workflowRun.head_branch,
-        headCommit: workflowRun.head_commit?.id || null,
-        runId: workflowRun.id,
-        status: workflowRun.status,
-        conclusion: workflowRun.conclusion,
-        commit: workflowRun.head_sha,
-        createdAt: workflowRun.created_at,
-        startedAt: workflowRun.run_started_at || null,
-        durationInMS: usage.run_duration_ms,
+      },
+    )) {
+      for (const workflow of response.data) {
+        yield workflow
       }
-
-      for (const key of Object.keys(usage.billable)) {
-        const os = key as (keyof typeof usage.billable)
-        const jobs = usage.billable[os]!.job_runs || []
-
-        for (const job of jobs) {
-          const jobRequest = await gh.client.actions.getJobForWorkflowRun({
-            owner: OWNER,
-            repo: REPO,
-            run_id: workflowRun.id,
-            job_id: job.job_id,
-          })
-          const jobData = jobRequest.data;
-
-          const jobLine = {
-            ...workflowLine,
-            os,
-            jobId: job.job_id,
-            jobDurationInMS: job.duration_ms || null,
-            jobName: jobData.name,
-            jobStartedAt: jobData.started_at,
-            jobCompletedAt: jobData.completed_at || null,
-          }
-          jobLines.push(jobLine)
-
-          for (const step of (jobData.steps || [])) {
-            const stepLine = {
-              ...jobLine,
-              stepStatus: step.status,
-              stepConclusion: step.conclusion,
-              stepName: step.name,
-              stepNumber: step.number,
-              stepStartedAt: step.started_at || null,
-              stepCompletedAt: step.completed_at || null,
-            }
-            stepLines.push(stepLine)
-
-            if (isDone()) {
-              break;
-            }
-          }
-
-          if (isDone()) {
-            break;
-          }
-        }
-
-        if (isDone()) {
-          break;
-        }
-      }
-
-      workflowLines.push(workflowLine)
-
-      if (isDone()) {
-        break;
-      }
-    }
-
-    if (isDone()) {
-      break;
     }
   }
 
-  console.log(JSON.stringify(stepLines));
+  async function* allRuns(workflowId: string | number) {
+    for await (const response of allOf(actions.listWorkflowRuns,
+      {
+        owner: OWNER,
+        repo: REPO,
+        branch: BRANCH,
+        workflow_id: workflowId,
+      })) {
+
+      for await (const workflowRun of response.data) {
+        yield workflowRun
+      }
+    }
+  }
+
+  try {
+
+    for await (const workflow of allWorkflows()) {
+      throwWhenDone();
+      debug.log(`Fetching workflow ${workflow.id} (${workflow.name})`)
+
+      for await (const workflowRun of allRuns(workflow.id)) {
+        throwWhenDone();
+        debug.log(`Fetching workflow run ${workflowRun.id} (${workflowLines.length})`)
+
+        const usageRequest = await actions.getWorkflowRunUsage({
+          owner: OWNER,
+          repo: REPO,
+          run_id: workflowRun.id,
+        })
+
+        const usage = usageRequest.data;
+
+        const workflowLine = {
+          workflowId: workflow.id,
+          workflowName: workflow.name,
+          headBranch: workflowRun.head_branch,
+          headCommit: workflowRun.head_commit?.id || null,
+          runId: workflowRun.id,
+          status: workflowRun.status,
+          conclusion: workflowRun.conclusion,
+          commit: workflowRun.head_sha,
+          createdAt: workflowRun.created_at,
+          startedAt: workflowRun.run_started_at || null,
+          durationInMS: usage.run_duration_ms,
+        }
+        workflowLines.push(workflowLine);
+
+        for (const key of Object.keys(usage.billable)) {
+          throwWhenDone();
+          const os = key as (keyof typeof usage.billable)
+          const jobs = usage.billable[os]!.job_runs || []
+
+          for (const job of jobs) {
+            const jobRequest = await actions.getJobForWorkflowRun({
+              owner: OWNER,
+              repo: REPO,
+              run_id: workflowRun.id,
+              job_id: job.job_id,
+            })
+
+            const jobData = jobRequest.data;
+
+            const jobLine = {
+              ...workflowLine,
+              os,
+              jobId: job.job_id,
+              jobDurationInMS: job.duration_ms || null,
+              jobName: jobData.name,
+              jobStartedAt: jobData.started_at,
+              jobCompletedAt: jobData.completed_at || null,
+            }
+            jobLines.push(jobLine)
+
+            for (const step of (jobData.steps || [])) {
+              throwWhenDone();
+              const stepLine = {
+                ...jobLine,
+                stepStatus: step.status,
+                stepConclusion: step.conclusion,
+                stepName: step.name,
+                stepNumber: step.number,
+                stepStartedAt: step.started_at || null,
+                stepCompletedAt: step.completed_at || null,
+              }
+              stepLines.push(stepLine)
+            }
+          }
+        }
+
+        debug.log(`Done fetching workflow run ${workflowRun.id}`)
+      }
+    }
+  } catch (e) {
+    if (e !== DoneErr) {
+      throw e
+    }
+  } finally {
+    console.log(JSON.stringify(stepLines));
+  }
 }
 
 run()
   .then(() => process.exit(0))
-  .catch(() => process.exit(1));
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  });
