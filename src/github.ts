b6e7d2677c1fbde79fc5ab747755b1c51d535d92
Laurent Senta <laurent@singulargarden.com>
2022-08-31 11:32:11 +0200

    basic: retrieve all stats and dump them on stdout

--- src/github.ts
+++ src/github.ts
@@ -1,41 +1,45 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 import * as core from '@actions/core'
-import {Octokit} from '@octokit/rest'
-import {graphql} from '@octokit/graphql'
-import {retry} from '@octokit/plugin-retry'
-import {throttling} from '@octokit/plugin-throttling'
-import {createAppAuth} from '@octokit/auth-app'
-import {env} from './utils'
-import {graphql as GraphQL} from '@octokit/graphql/dist-types/types' // eslint-disable-line import/no-unresolved
-import {GetResponseDataTypeFromEndpointMethod} from '@octokit/types' // eslint-disable-line import/named
+import { createTokenAuth } from '@octokit/auth-token'
+import { graphql } from '@octokit/graphql'
+import { graphql as GraphQL } from '@octokit/graphql/dist-types/types'; // eslint-disable-line import/no-unresolved
+import { retry } from '@octokit/plugin-retry'
+import { throttling } from '@octokit/plugin-throttling'
+import { Octokit } from '@octokit/rest'
+import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types'; // eslint-disable-line import/named
 
 const Client = Octokit.plugin(retry, throttling)
+
 const Endpoints = new Octokit()
+
 type Repositories = GetResponseDataTypeFromEndpointMethod<
   typeof Endpoints.repos.listForOrg
 >
+
 type Teams = GetResponseDataTypeFromEndpointMethod<typeof Endpoints.teams.list>
+
+const GH_TOKEN = process.env.GH_TOKEN
+
+if (!GH_TOKEN) {
+  throw `missing GH_TOKEN`
+}
 
 export class GitHub {
   static github: GitHub
-  static async getGitHub(): Promise<GitHub> {
+
+  static async get(): Promise<GitHub> {
     if (GitHub.github === undefined) {
-      const auth = createAppAuth({
-        appId: env.GITHUB_APP_ID,
-        privateKey: env.GITHUB_APP_PEM_FILE
-      })
-      const installationAuth = await auth({
-        type: 'installation',
-        installationId: env.GITHUB_APP_INSTALLATION_ID
-      })
-      GitHub.github = new GitHub(installationAuth.token)
+      const auth = createTokenAuth(GH_TOKEN!);
+      const { token } = await auth();
+      GitHub.github = new GitHub(token)
     }
+
     return GitHub.github
   }
 
   client: Octokit
-  graphqlClient: GraphQL
+  graphql: GraphQL
 
   private constructor(token: string) {
     this.client = new Client({
@@ -43,7 +47,7 @@ export class GitHub {
       throttle: {
         onRateLimit: (
           retryAfter: number,
-          options: {method: string; url: string; request: {retryCount: number}}
+          options: { method: string; url: string; request: { retryCount: number } }
         ) => {
           core.warning(
             `Request quota exhausted for request ${options.method} ${options.url}`
@@ -57,7 +61,7 @@ export class GitHub {
         },
         onSecondaryRateLimit: (
           retryAfter: number,
-          options: {method: string; url: string; request: {retryCount: number}}
+          options: { method: string; url: string; request: { retryCount: number } }
         ) => {
           core.warning(
             `SecondaryRateLimit detected for request ${options.method} ${options.url}`
@@ -71,148 +75,11 @@ export class GitHub {
         }
       }
     })
-    this.graphqlClient = graphql.defaults({
+
+    this.graphql = graphql.defaults({
       headers: {
         authorization: `token ${token}`
       }
     })
-  }
-
-  async listMembers() {
-    core.info('Listing members...')
-    return this.client.paginate(this.client.orgs.listMembers, {
-      org: env.GITHUB_ORG
-    })
-  }
-
-  private repositories?: Repositories
-  async listRepositories() {
-    if (!this.repositories) {
-      core.info('Listing repositories...')
-      this.repositories = await this.client.paginate(
-        this.client.repos.listForOrg,
-        {
-          org: env.GITHUB_ORG
-        }
-      )
-    }
-    return this.repositories
-  }
-
-  private teams?: Teams
-  async listTeams() {
-    if (!this.teams) {
-      core.info('Listing teams...')
-      this.teams = await this.client.paginate(this.client.teams.list, {
-        org: env.GITHUB_ORG
-      })
-    }
-    return this.teams
-  }
-
-  async listRepositoryCollaborators() {
-    const repositoryCollaborators = []
-    const repositories = await this.listRepositories()
-    for (const repository of repositories) {
-      core.info(`Listing ${repository.name} collaborators...`)
-      const collaborators = await this.client.paginate(
-        this.client.repos.listCollaborators,
-        {owner: env.GITHUB_ORG, repo: repository.name, affiliation: 'direct'}
-      )
-      repositoryCollaborators.push(
-        ...collaborators.map(collaborator => ({repository, collaborator}))
-      )
-    }
-    return repositoryCollaborators
-  }
-
-  async listRepositoryBranchProtectionRules() {
-    // https://github.com/octokit/graphql.js/issues/61
-    const repositoryBranchProtectionRules = []
-    const repositories = await this.listRepositories()
-    for (const repository of repositories) {
-      core.info(`Listing ${repository.name} branch protection rules...`)
-      const {
-        repository: {
-          branchProtectionRules: {nodes}
-        }
-      }: {repository: {branchProtectionRules: {nodes: {pattern: string}[]}}} =
-        await this.graphqlClient(
-          `
-          {
-            repository(owner: "${env.GITHUB_ORG}", name: "${repository.name}") {
-              branchProtectionRules(first: 100) {
-                nodes {
-                  pattern
-                }
-              }
-            }
-          }
-        `
-        )
-      repositoryBranchProtectionRules.push(
-        ...nodes.map(node => ({repository, branchProtectionRule: node}))
-      )
-    }
-    return repositoryBranchProtectionRules
-  }
-
-  async listTeamMembers() {
-    const teamMembers = []
-    const teams = await this.listTeams()
-    for (const team of teams) {
-      core.info(`Listing ${team.name} members...`)
-      const members = await this.client.paginate(
-        this.client.teams.listMembersInOrg,
-        {org: env.GITHUB_ORG, team_slug: team.slug}
-      )
-      teamMembers.push(...members.map(member => ({team, member})))
-    }
-    return teamMembers
-  }
-
-  async listTeamRepositories() {
-    const teamRepositories = []
-    const teams = await this.listTeams()
-    for (const team of teams) {
-      core.info(`Listing ${team.name} repositories...`)
-      const repositories = await this.client.paginate(
-        this.client.teams.listReposInOrg,
-        {org: env.GITHUB_ORG, team_slug: team.slug}
-      )
-      teamRepositories.push(
-        ...repositories.map(repository => ({team, repository}))
-      )
-    }
-    return teamRepositories
-  }
-
-  async getRepositoryFile(repository: string, path: string) {
-    core.info(`Checking if ${repository}/${path} exists...`)
-    try {
-      const repo = (
-        await this.client.repos.get({
-          owner: env.GITHUB_ORG,
-          repo: repository
-        })
-      ).data
-      if (repo.owner.login === env.GITHUB_ORG && repo.name === repository) {
-        return (
-          await this.client.repos.getContent({
-            owner: env.GITHUB_ORG,
-            repo: repository,
-            path
-          })
-        ).data as {path: string; url: string}
-      } else {
-        core.debug(
-          `${env.GITHUB_ORG}/${repository} has moved to ${repo.owner.login}/${repo.name}`
-        )
-        return undefined
-      }
-    } catch (e) {
-      core.debug(JSON.stringify(e))
-      return undefined
-    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,183 @@
+import { Console } from 'console'
 import 'reflect-metadata'
+import { GitHub } from './github'
 
+const OWNER = 'laurentsenta'
+const REPO = 'test-plans'
+const BRANCH = 'laurent-statistics-3008'
+const RUNS_LIMIT = 2000
+const DEBUG = false
+
+interface IWorkflowLine {
+  workflowId: number,
+  workflowName: string,
+  headBranch: string | null,
+  headCommit: string | null,
+  runId: number,
+  status: string | null,
+  conclusion: string | null,
+  commit: string,
+  createdAt: string,
+  startedAt: string | null
+}
+
+interface IJobLine extends IWorkflowLine {
+  os: string,
+  jobId: number,
+  jobDurationInMS: number | null,
+  jobStartedAt: string,
+  jobCompletedAt: string | null,
+}
+
+interface IJobStepLine extends IJobLine {
+  stepStatus: string,
+  stepConclusion: string | null,
+  stepName: string,
+  stepNumber: number,
+  stepStartedAt: string | null,
+  stepCompletedAt: string | null,
+}
+
+const debug = new Console(process.stderr)
+
+// https://docs.github.com/en/rest/actions/workflow-runs#get-workflow-run-usage
 async function run(): Promise<void> {
-  console.log('helloworld')
+  const gh = await GitHub.get()
+
+  const workflowLines: IWorkflowLine[] = []
+  const jobLines: IJobLine[] = []
+  const stepLines: IJobStepLine[] = []
+
+  // Naive debug implementation, TODO: throw or use generators instead.
+  const isDone = (): boolean => {
+    if (!DEBUG) {
+      return false;
+    }
+
+    if (jobLines.length > 10 || workflowLines.length > 10) {
+      return true; // debug with 10 lines for now
+    }
+    if (stepLines.length > 10) {
+      return true; // debug with 10 lines for now
+    }
+
+    return false;
+  }
+
+  const workflowsRequest = await gh.client.actions.listRepoWorkflows({
+    owner: OWNER,
+    repo: REPO,
+  })
+
+  const workflows = workflowsRequest.data.workflows;
+  debug.log(`Found ${workflows.length} workflows.`)
+
+  for (const workflow of workflows) {
+    debug.log(`Fetching workflow ${workflow.id} (${workflow.name})`)
+
+    const workflowRunsRequest = await gh.client.actions.listWorkflowRuns({
+      owner: OWNER,
+      repo: REPO,
+      branch: BRANCH,
+      workflow_id: workflow.id,
+      limit: RUNS_LIMIT,
+    })
+    const workflowRuns = workflowRunsRequest.data.workflow_runs;
+
+    debug.log(`Found ${workflowRuns.length} workflow runs.`)
+    debug.log("a workflow looks like this:", workflowRuns[0])
+
+    for (const workflowRun of workflowRuns) {
+      debug.log(`Fetching run ${workflowRun.id}`)
+
+      const usageRequest = await gh.client.actions.getWorkflowRunUsage({
+        owner: OWNER,
+        repo: REPO,
+        run_id: workflowRun.id,
+      })
+
+      const usage = usageRequest.data;
+
+      const workflowLine = {
+        workflowId: workflow.id,
+        workflowName: workflow.name,
+        headBranch: workflowRun.head_branch,
+        headCommit: workflowRun.head_commit?.id || null,
+        runId: workflowRun.id,
+        status: workflowRun.status,
+        conclusion: workflowRun.conclusion,
+        commit: workflowRun.head_sha,
+        createdAt: workflowRun.created_at,
+        startedAt: workflowRun.run_started_at || null,
+        durationInMS: usage.run_duration_ms,
+      }
+
+      for (const key of Object.keys(usage.billable)) {
+        const os = key as (keyof typeof usage.billable)
+        const jobs = usage.billable[os]!.job_runs || []
+
+        for (const job of jobs) {
+          const jobRequest = await gh.client.actions.getJobForWorkflowRun({
+            owner: OWNER,
+            repo: REPO,
+            run_id: workflowRun.id,
+            job_id: job.job_id,
+          })
+          const jobData = jobRequest.data;
+
+          const jobLine = {
+            ...workflowLine,
+            os,
+            jobId: job.job_id,
+            jobDurationInMS: job.duration_ms || null,
+            jobName: jobData.name,
+            jobStartedAt: jobData.started_at,
+            jobCompletedAt: jobData.completed_at || null,
+          }
+          jobLines.push(jobLine)
+
+          for (const step of (jobData.steps || [])) {
+            const stepLine = {
+              ...jobLine,
+              stepStatus: step.status,
+              stepConclusion: step.conclusion,
+              stepName: step.name,
+              stepNumber: step.number,
+              stepStartedAt: step.started_at || null,
+              stepCompletedAt: step.completed_at || null,
+            }
+            stepLines.push(stepLine)
+
+            if (isDone()) {
+              break;
+            }
+          }
+
+          if (isDone()) {
+            break;
+          }
+        }
+
+        if (isDone()) {
+          break;
+        }
+      }
+
+      workflowLines.push(workflowLine)
+
+      if (isDone()) {
+        break;
+      }
+    }
+
+    if (isDone()) {
+      break;
+    }
+  }
+
+  console.log(JSON.stringify(stepLines));
 }
 
 run()
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));
